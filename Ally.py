0547d40067aa11398b8c3d053a2f68df27cbab43
Ally Kimble <ak126472@student.wpsok.org>
2026-04-21 14:10:08 -0500

    First day

--- Ally.py
+++ Ally.py
@@ -41,3 +41,40 @@ while player.health > 0 and day <= 7:
     print("4. Rest")
     print("5. Eat the zombie")
     print("6. View inventory")
+
+    choice = input("Enter your choice(1-5): ")
+
+    if choice == "1":
+        find = random.randint(1, 3)
+
+        if find == 1:
+            print("You found some lettuce!")
+            player.food +=  1
+        elif find == 2:
+            print("You found the Fork of Wonder!")
+            player.weapon = "Fork of Wonder"
+        else:
+            print("A zombie was inside the house and you got attacked!!! :O")
+            player.health -= 25
+
+    if choice == "2":
+        damage = random.randint(5, 30)
+
+        if player.weapon == "Fork of Wonder":
+            damage -= 5
+
+        print("You fought a zombie!!")
+        print("You lost,", damage, "health! :(")
+        player.health -= damage
+
+    elif choice == "3":
+        if player.food >0:
+            print("You ate some scrumptious lettuce!")
+            player.food -= 1
+            player.health += 2
+        else:
+            print("No food left! T-T")
+
+    elif choice == "4":
+        print("You rested and gained health")
+        player.health += 5
