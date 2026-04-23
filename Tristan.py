325cd4b9e1f1d7dc0d5f5d6a048f204c79be2e91
Tristan Long <tl129267@studrnt.wpsok.org>
2026-04-23 14:09:36 -0500

    boos

--- Tristan.py
+++ Tristan.py
@@ -1,12 +1,26 @@
 import random
 event = random.randint(1, 2)
 
-event_list
 
 def zombie_event():
-        print("An EVENT is coming")
+        print("An Event is on it's way")
         if event == 1:
-            print("Event 1 is coming")
+            print("Event1:Zombie_rush has appeared")
         elif event == 2:
-            print("Event 2 is coming")
+            print("Event2:Zombie_Boss has appeared")
 zombie_event()
+
+def secret_event():
+        print("secret event has appeared")
+        if event == 1:
+            print("you completed the secret event")
+        elif event == 2:
+            print("you filed the secret event")
+secret_event()
+
+class Zombie_Boss:
+    def __init__(self, hp, speed, damage):
+        self.hp = hp
+        self.speed = speed
+        self.damage = damage
+boss = Zombie_Boss(150, 5, 15)
