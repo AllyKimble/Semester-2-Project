464f1342e66a30665d79879ba3468ea5fe8e9dc8
Ally Kimble <ak126472@student.wpsok.org>
2026-04-27 14:07:52 -0500

    Finished Day 2. Need to add golden key.

--- Ally.py
+++ Ally.py
@@ -13,7 +13,7 @@ class Player:
         self.health = 100
         self.food = 1
         self.weapon = "Fists"
-        self.inventory = ["Map", "Flashlight", "Socks", "Water Flask"]
+        self.inventory = ["Flashlight", "Socks", "Water Flask"]
 
 
 name = input("Enter your survivor's name: ")
@@ -183,6 +183,7 @@ while player.health > 0 and day == 2:
         print("Health: ", player.health)
 
     if choice == "3":
+        choices_made += 1
         print("You decided to set up camp!")
         a = random.randint(1, 10)
         if a == 1:
@@ -218,7 +219,100 @@ while player.health > 0 and day == 2:
             print("Health: ", player.health)
         elif a == 9:
             print("You hear a voice from the foliage. You walk over and find a small, purple cricket who is wearing a top hat, and a small pair of golden socks. It beckons you over, and hands you a small golden key. It looks you in the eyes, before jumping into the fire with a small scream. You pocket the key.")
-            player.inventory.append(player.inventory.pop("Golden Key"))
+            #NEED TO ADD GOLDEN KEY#
+        elif a == 10:
+            print("You attracted a zombie horde!")
+            player.health -= random.randint(20, 50)
+            print("Health: ", player.health)
 
+    if choice == "4":
+        choices_made += 1
+        b = random.randint(1, 7)
+        if b == 1:
+            print("You search the surrounding forest for humans. You find nothing.")
+        if b == 2:
+            print("You search the surrounding forest for humans. You find an old campfire, and some footprints leading towards the town.")
+        if b == 3:
+            print("You call out, but only the wind answers... It's quiet.... Too quiet..")
+            c = random.randint(1,2)
+            if c == 1:
+                print("You continue on.")
+            elif c == 2:
+                print("A zombie appears from behind a tree! You get attacked.")
+                player.health -= random.randint(5, 20)
+                print("Health: ", player.health)
+        if b == 4:
+            print("You find a dropped backpack with a map marked “SAFE?”")
+            player.inventory.append(player.inventory.pop("Map"))
+        if b == 5:
+            print("Someone signals you from a distance but runs off before you get close.")
+            d = random.randint(1,2)
+            if d == 1:
+                print("You decide to go back to camp.")
+            elif d == 2:
+                print("You decide to take a closer look.")
+                print("Oh no! You fall in into a pit trap! You're injured, but manage to climb out.")
+                player.health -= 10
+                print("Health: ", player.health)
+        if b == 6:
+            print("You don't find people, but find a snowflake. Winter is coming.")
+        if b == 7:
+            print("You find a survivor, but they’re bitten.")
+            e = random.randint(1,3)
+            if e == 1:
+                print("You decide to leave them behind.")
+                f = random.randint(1,2)
+                if f == 1:
+                    print("you hear their cries, but don't turn back.")
+                elif f == 2:
+                    print("They jump forward and attack you from behind!")
+                    player.health -= random.randint(5, 20)
+                    print("Health: ", player.health)
+            if e == 2:
+                print("You decide to try and help them.")
+                print("They lose control and attack you!")
+                player.health -= random.randint(5, 20)
+                print("Health: ", player.health)
+            if e == 3:
+                print("You put them out of their misery, but before you do, they manage to whisper to you. 'Find the socks, and save the world'")
 
+    if choice == "5":
+        choices_made += 1
+        print("You decide to play dress to impress, but there is no internet and your phone dies while it was loading.")
 
+    if choice == "6":
+        choices_made += 1
+        g = random.randint(1,5)
+        if g == 1:
+            print("You decide to plant lettuce!")
+            player.food += 5
+            print("Food: ", player.food)
+        if g == 2:
+            print("You decide to plant potatoes!")
+            player.food += 5
+            print("Food: ", player.food)
+        if g == 3:
+            print("You decide to plant corn!")
+            player.food += 5
+            print("Food: ", player.food)
+        if g == 4:
+            print("You decide to plant butter!")
+            player.food += 20
+            print("Food: ", player.food)
+        if g == 5:
+            print("You decide to plant carrots!")
+            player.food += 5
+            print("Food: ", player.food)
+
+    if choice == "7":
+        choices_made += 1
+        print("You decide to go for a run.")
+        h = random.randint(1,5)
+        if h == 1:
+            print("You run into some zombies!")
+            player.health -= random.randint(5, 20)
+            print("Health: ", player.health)
+        elif h == 2:
+            print("You have a nice refreshing run.")
+            player.health += 10
+            print("Health: ", player.health)
