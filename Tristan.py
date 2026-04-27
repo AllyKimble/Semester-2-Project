ce31f5218f2ce1d28259e94b7a9f002410f24ed4
Tristan Long <tl129267@studrnt.wpsok.org>
2026-04-27 13:29:16 -0500

    no boss day 4

--- Tristan.py
+++ Tristan.py
@@ -7,7 +7,7 @@ def zombie_event():
         if event == 1:
             print("Event1:Zombie_rush has appeared")
         elif event == 2:
-            print("Event2:Zombie_Boss has appeared")
+            print("Event2:Zombie_horde has appeared")
 zombie_event()
 
 def secret_event():
@@ -17,15 +17,3 @@ def secret_event():
         elif event == 2:
             print("you filed the secret event")
 secret_event()
-#big boss on day what ever
-class Zombie_Boss:
-    def __init__(self, hp, speed, damage):
-        self.hp = hp
-        self.speed = speed
-        self.damage = damage
-boss = Zombie_Boss(150, 5, 15)
-print('zombie_boss')
-print(boss.hp)
-print(boss.speed)
-print(boss.damage)
-#NEED TO MAKE BOSS SPAWN RANDOM
