3739c432ba74d90bf454adce74e7a1e8989024c1
Tristan Long <tl129267@studrnt.wpsok.org>
2026-05-07 14:09:47 -0500

    needs work

--- Tristan's test.py
+++ Tristan's test.py
@@ -1,6 +1,16 @@
 #day 6
-def daysix():
-    print('its . finally . day . six . you . can . work . on . getting . help . off . the . GOOD . eye-land .')
-    print('its . where . BAD . BAD . BADDDDD . stuff . happen . and . it . could . be . a . HAPPY . PLAYS . where . you . can . make . zombie . friends .')
-    print('LETS  GO  GET  REEDY  KNOW . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! .')
-daysix()
+import random
+
+def instructions():
+    global instructions
+    instructions = random.randint(1, 2)
+    print('morning')
+    if instructions == 1:
+        print('not day 6 yet')
+    elif instructions == 2:
+        print('its day 6')
+    if instructions == 2:
+        print('its . finally . day . six . you . can . work . on . getting . help . off . the . GOOD . eye-land .')
+        print('its . where . BAD . BAD . BADDDDD . stuff . happen . and . it . could . be . a . HAPPY . PLAYS . where . you . can . make . zombie . friends .')
+        print('LETS  GO  GET  REEDY  KNOW . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! .')
+instructions()
--- Tristan.py
+++ Tristan.py
@@ -25,9 +25,19 @@ def secret_event1():
         print("secret event is here")
         secret_event2()
 secret_event1()
+#day 6
+import random
 
-class jwi:
-    def __init__(self, hp, damage, weight):
-        self.hp = hp
-        self.damage = damage
-        self.weight = weight
+def instructions():
+    global instructions
+    instructions = random.randint(1, 2)
+    print('morning')
+    if instructions == 1:
+        print('not day 6 yet')
+    elif instructions == 2:
+        print('its day 6')
+    if instructions == 2:
+        print('its . finally . day . six . you . can . work . on . getting . help . off . the . GOOD . eye-land .')
+        print('its . where . BAD . BAD . BADDDDD . stuff . happen . and . it . could . be . a . HAPPY . PLAYS . where . you . can . make . zombie . friends .')
+        print('LETS  GO  GET  REEDY  KNOW . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! .')
+instructions()
