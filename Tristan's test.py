d4da4ff735e9f20585f9c5a90f501f4f54db77b6
Tristan Long <tl129267@studrnt.wpsok.org>
2026-05-07 13:40:06 -0500

    needs work

--- Tristan's test.py
+++ Tristan's test.py
@@ -1,6 +1,6 @@
 #day 6
 def daysix():
-    print('its finally day six you can work on getting help off the GOOD eye-land ')
-    print('its where BAD BAD BADDDDD stuff happen and it could be a HAPPY PLAYS where you can get friends')
-    print('LETS GO GET REEDY KNOW')
+    print('its . finally . day . six . you . can . work . on . getting . help . off . the . GOOD . eye-land .')
+    print('its . where . BAD . BAD . BADDDDD . stuff . happen . and . it . could . be . a . HAPPY . PLAYS . where . you . can . make . zombie . friends .')
+    print('LETS  GO  GET  REEDY  KNOW . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! . ! .')
 daysix()
